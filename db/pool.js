7a9400380441cc392f69ff20aa8ff0df2e4a4bf6
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-09-06 11:49:42 -0500

    build: add sslmode for Neon database

--- db/pool.js
+++ db/pool.js
@@ -3,9 +3,14 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 module.exports = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString:
+    "postgresql://" +
+    process.env.DB_USER +
+    ":" +
+    process.env.DB_PASSWORD +
+    "@" +
+    process.env.DB_HOST +
+    "/" +
+    process.env.DB_NAME +
+    "?sslmode=require&channel_binding=require",
 });
